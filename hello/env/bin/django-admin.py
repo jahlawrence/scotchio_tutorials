1ad58f5b1d752e2be62e26368c3df6f3dcea895c
jason <jasgriff100@gmail.com>
2017-06-09 13:44:12 +0100

    commit first django tutorial

---
+++ hello/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/iman/Python_Environments/scotchio/projects/hello/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
