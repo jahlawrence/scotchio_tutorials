a05d8cfb355fd520616afa92b2a9f224a5cbf2ad
jason <jasgriff100@gmail.com>
2017-06-15 11:45:55 +0100

    Beginner CRUD Web App

--- learning_flask/config.py
+++ learning_flask/config.py
@@ -1,5 +1,34 @@
 # config.py one variable per line
 
-# enable debug. make sure its false in production 
+# if enable debug. make sure its false in production 
+# Put any configurations here that are common across all environments
 
-DEBUG = True
+class Config(object):
+    """
+    Common Configs
+    """
+
+class DevelopmentConfig(Config):
+    """
+    Dev Configs
+    """
+
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+
+    DEBUG = False
+  
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+
+}
